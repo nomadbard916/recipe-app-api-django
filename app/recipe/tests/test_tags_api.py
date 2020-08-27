06be4735fc3057ab559396205ab11d623155d6ed
nomadbard916 <neonnova916@gmail.com>
2020-08-27 13:15:39 +0800

    added list ingredients feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -76,3 +76,4 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from . import serializers
 
@@ -36,3 +36,10 @@ class TagViewSet(BaseRecipeAttrViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
