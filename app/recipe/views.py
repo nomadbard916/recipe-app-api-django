593146eab51a8d5855357ac46589d1414402bae7
nomadbard916 <neonnova916@gmail.com>
2020-08-28 13:30:53 +0800

    added list recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from . import serializers
 
@@ -43,3 +43,30 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        # tags = self.request.query_params.get("tags")
+        # ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        # if tags:
+        #     tag_ids = self._params_to_ints(tags)
+        #     queryset = queryset.filter(tags__id__in=tag_ids)
+        # if ingredients:
+        #     ingredient_ids = self._params_to_ints(ingredients)
+        #     queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
